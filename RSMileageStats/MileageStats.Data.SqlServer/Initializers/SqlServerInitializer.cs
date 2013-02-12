7e2be878042184a0ca6f2c6f28b439295921bd44
Patrick McClory <pmcclory@gmail.com>
2013-02-12 09:39:11 -0500

    Committing initial revision of building project
    Project upgraded to VS 2012 U1 and added two projects to hold build
    process.  Still need to perform a number of updates to underlying code.

---
+++ RSMileageStats/MileageStats.Data.SqlServer/Initializers/SqlServerInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace MileageStats.Data.SqlServer.Initializers
+{
+    public abstract class SqlServerInitializer<T> : IDatabaseInitializer<T> where T : DbContext
+    {
+    }
+}
